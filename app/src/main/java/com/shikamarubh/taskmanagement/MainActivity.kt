cb4f01c59a2a99249eff03e5d6c8957946af344d
Lê Mạnh Duy <lemanhduy311111@gmail.com>
2022-05-27 20:24:09 +0700

    feat(8) : code ui giao diện màn hình chính, các màn hình task, màn hình archive, màn hình trash

--- app/src/main/java/com/shikamarubh/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/shikamarubh/taskmanagement/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -14,8 +16,10 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.shikamarubh.taskmanagement.ui.theme.TaskManagementTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -42,57 +47,164 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+//Màn hình chính
 @Preview
 @Composable
-//Màn hình chính
-fun GetScaffold(){
+fun MainScreen(){
     Scaffold(
         topBar = { TopAppBar() },
         content = {
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically) {
-                CardProject()
-                CardProject()
-            }
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically){
+                    CardProject()
+                    CardProject()
+                }
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically){
                     ButtonAddProject()
                 }
+            }
         },
         backgroundColor = Color(251, 249, 245),
     )
 }
 
 
-//Thanh top menu
+//Màn hình Task
+@Preview
 @Composable
-fun TopAppBar() {
-    val expanded = remember { mutableStateOf(false)}
+fun TaskScreen(){
+    Scaffold(
+        topBar = { TopAppBar() },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally) {
 
-    TopAppBar(
-        title = { Text(text = "Task Manager App", fontWeight = FontWeight.Bold, color = Color(125, 118, 102)) },
-        backgroundColor = Color(255, 255, 255),
-        navigationIcon = {
-            IconButton(onClick = {
-                // do something here
-            }) {
-                Icon(
-                    Icons.Filled.Menu,
-                    contentDescription = "Localized description"
-                )
+                Column() {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically) {
+                        ButtonToDoChecked()
+                        ButtonDone()
+                        ButtonDoing()
+                    }
+
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically){
+                        CardTask()
+                    }
+                }
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically){
+                    ButtonAddTask()
+                }
             }
         },
+        backgroundColor = Color(251, 249, 245),
     )
+}
+
+//Màn hình Archive
+@Preview
+@Composable
+fun ArchiveScreen(){
+    Scaffold(
+        topBar = { TopAppBar() },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically){
+                    CardProject()
+                }
+
+            }
+        },
+        backgroundColor = Color(251, 249, 245),
+    )
+}
+
+//Màn hình thùng rác
+@Preview
+@Composable
+fun TrashScreen(){
+    val state = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = state,
+        topBar = { TopAppBar() },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically){
+                    CardProject()
+                    CardProject()
+                }
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically){
+                    ButtonDeleteAllProject()
+                }
+            }
+        },
+        backgroundColor = Color(251, 249, 245),
+    )
+}
+
+//Card task
+@Composable
+fun CardTask() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        modifier = paddingModifier,
+    ) {
+        Column(modifier = paddingModifier) {
+            Text(text = "Crawl dữ liệu", fontSize = 10.sp, fontWeight = FontWeight.Bold,color=Color.Black)
+        }
+    }
 }
 
 //Card dự án
 @Composable
 fun CardProject() {
-    val paddingModifier = Modifier.padding(19.dp)
+    val paddingModifier = Modifier.padding(17.dp)
     val expanded = remember { mutableStateOf(false)}
     Card(
         elevation = 10.dp,
@@ -106,12 +218,135 @@ fun CardProject() {
     }
 }
 
-//Nút xoá dự án trong thùng rác
+//Thanh top menu
 @Composable
-fun ButtonDeleteAllProject() {
-    Button(onClick = {}, shape = RoundedCornerShape(25.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color(245, 62, 62))) {
-        Text("-", fontSize = 18.sp, fontWeight = FontWeight.Bold,color=Color.White)
-        Text(text = " DELETE ALL",color=Color.White)
+fun TopAppBar() {
+    TopAppBar(
+        title = { Text(text = "Task Manager App", fontWeight = FontWeight.Bold, color = Color(125, 118, 102)) },
+        backgroundColor = Color(255, 255, 255),
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/
+            }) {
+                Icon(
+                    Icons.Filled.Menu,
+                    contentDescription = "Localized description",
+                )
+            }
+        }
+    )
+}
+
+//Button To-do
+@Composable
+fun ButtonToDo() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        backgroundColor = Color(125, 117, 109)
+    ) {
+        Row(modifier = paddingModifier) {
+            Text(text = "TO-DO ", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+            Text(text = " 24", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+        }
+    }
+}
+
+//Button To-do Checked
+@Composable
+fun ButtonToDoChecked() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        modifier = paddingModifier.border(3.dp, Color(146, 180, 236),shape= RoundedCornerShape(50)),
+        backgroundColor = Color(125, 117, 109)
+    ) {
+        Row(modifier = paddingModifier) {
+            Text(text = "TO-DO ", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+            Text(text = " 24", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+        }
+    }
+}
+
+//Button Doing
+@Composable
+fun ButtonDoing() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        backgroundColor = Color(245, 160, 62)
+    ) {
+        Row(modifier = paddingModifier) {
+            Text(text = "DOING ", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+            Text(text = " 24", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+        }
+    }
+}
+
+//Button Doing Checked
+@Composable
+fun ButtonDoingChecked() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        modifier = paddingModifier.border(3.dp, Color(146, 180, 236),shape= RoundedCornerShape(50)),
+        backgroundColor = Color(245, 160, 62)
+    ) {
+        Row(modifier = paddingModifier) {
+            Text(text = "DOING ", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+            Text(text = " 24", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+        }
+    }
+}
+
+//Button Done
+@Composable
+fun ButtonDone() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        backgroundColor = Color(139, 192, 106)
+    ) {
+        Row(modifier = paddingModifier) {
+            Text(text = "DONE ", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+            Text(text = " 24", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+        }
+    }
+}
+
+//Button Done Checked
+@Composable
+fun ButtonDoneChecked() {
+    val paddingModifier = Modifier.padding(10.dp)
+    val expanded = remember { mutableStateOf(false)}
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(50),
+        modifier = paddingModifier.border(3.dp, Color(146, 180, 236),shape= RoundedCornerShape(50)),
+        backgroundColor = Color(139, 192, 106)
+    ) {
+        Row(modifier = paddingModifier) {
+            Text(text = "DONE ", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+            Text(text = " 24", fontSize = 14.sp, fontWeight = FontWeight.ExtraBold,color=Color.White)
+        }
+    }
+}
+
+//Nút thêm dự án
+@Composable
+fun ButtonAddProject() {
+    Button(onClick = {}, shape = RoundedCornerShape(25.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color(98, 98, 246))) {
+        Text("+", fontSize = 18.sp, fontWeight = FontWeight.Bold,color=Color.White)
+        Text(text = " ADD PROJECT",color=Color.White)
     }
 }
 
@@ -120,17 +355,16 @@ fun ButtonDeleteAllProject() {
 fun ButtonAddTask() {
     Button(onClick = {}, shape = RoundedCornerShape(25.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color(98, 98, 246))) {
         Text("+", fontSize = 18.sp, fontWeight = FontWeight.Bold,color=Color.White)
-        Text(text = " ADD Task",color=Color.White)
+        Text(text = " ADD TASK",color=Color.White)
     }
 }
 
-
-//Nút thêm dự án
+//Nút xoá dự án trong thùng rác
 @Composable
-fun ButtonAddProject() {
-    Button(onClick = {}, shape = RoundedCornerShape(25.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color(98, 98, 246))) {
-        Text("+", fontSize = 18.sp, fontWeight = FontWeight.Bold,color=Color.White)
-        Text(text = " ADD PROJECT",color=Color.White)
+fun ButtonDeleteAllProject() {
+    Button(onClick = {}, shape = RoundedCornerShape(25.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color(245, 62, 62))) {
+        Text("-", fontSize = 18.sp, fontWeight = FontWeight.Bold,color=Color.White)
+        Text(text = " DELETE ALL",color=Color.White)
     }
 }
 
