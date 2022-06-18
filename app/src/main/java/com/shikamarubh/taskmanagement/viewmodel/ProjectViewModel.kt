9d2bfff0281355d11e15cad8d7fd7e1689eb4682
ShikamaruBH <huynhphuquy712001asus4.4.2@gmail.com>
2022-06-18 17:14:13 +0700

    feat(26): firebase - Add comments

--- app/src/main/java/com/shikamarubh/taskmanagement/viewmodel/ProjectViewModel.kt
+++ app/src/main/java/com/shikamarubh/taskmanagement/viewmodel/ProjectViewModel.kt
@@ -22,7 +22,9 @@ import javax.inject.Inject
 @HiltViewModel
 class ProjectViewModel
     @Inject constructor(private val projectRepository: ProjectRepository) : ViewModel() {
+        // Đối tượng firestore dùng để giao tiếp với Firestore
         private val db = Firebase.firestore
+        // Đối tượng collection reference kết nối đến collection tương ứng trên Firestore
         private val collRef = db.collection("projects")
         private val auth = Firebase.auth
         private val _projectList = MutableStateFlow<List<Project>>(emptyList())
@@ -72,15 +74,18 @@ class ProjectViewModel
 
         fun addProject(project: Project) = viewModelScope.launch {
             if (auth.currentUser != null) {
+                // Tạo document mới trong collection projects
                 val docRef = collRef.document()
                 project.userId = auth.currentUser!!.uid
                 project.id = docRef.id
+                // Gán data trong document mới tạo bằng đối tượng project
                 docRef.set(project)
                 projectRepository.addProject(project)
             }
         }
         fun updateProject(project: Project) = viewModelScope.launch { projectRepository.updateProject(project) }
         fun archiveProject(project: Project) = viewModelScope.launch {
+            // Update trường isArchived với giá trị true trong collection projects (collRef) ở document có id bằng id của đối tượng project
             collRef.document(project.id).update("isArchived",true)
             projectRepository.archiveProject(project)
         }
@@ -88,6 +93,7 @@ class ProjectViewModel
         fun sortDeleteProject(project: Project) = viewModelScope.launch { projectRepository.sortDeleteProject(project) }
         fun restoreProject(project: Project) = viewModelScope.launch { projectRepository.restoreProject(project) }
         fun deleteProject(project: Project) = viewModelScope.launch {
+            // Xoá document trong collection projects mà có id bằng id của đối tượng project truyền vào
             collRef.document(project.id).delete()
             projectRepository.deleteProject(project)
         }
